ca2fdebc05d1df22dee3c2c5f3b81399f937a9d2
Poxie <poxen12@hotmail.com>
2021-08-11 01:36:15 +0200

    Removed temporary JSON files, moved to API calls

--- src/pages/home/TopTracks.tsx
+++ src/pages/home/TopTracks.tsx
@@ -3,32 +3,30 @@ import { useAPI } from "../../contexts/ApiProvider"
 import { Track } from "../../types/Track";
 import { AllTops } from "./AllTops";
 import { MostLiked } from "./MostLiked";
-import tempTracks from './TempTops.json';
 
 export const TopTracks = () => {
     const { get } = useAPI();
     // @ts-ignore
-    const [tracks, setTracks] = useState<Track[]>(tempTracks.reverse());
+    const [tracks, setTracks] = useState<Track[]>([]);
 
     useEffect(() => {
         // Fetching global top 50s
-        // get(`playlists/37i9dQZEVXbMDoHDwVN2tF`)
-        //     .then(res => res.json())
-        //     .then(response => {
-        //         const tracks = response.tracks.items.map((item: any) => item.track);
-        //         console.log(JSON.stringify(tracks));
-        //         setTracks(tracks);
-        //     })
+        get(`playlists/37i9dQZEVXbMDoHDwVN2tF`)
+            .then(res => res.json())
+            .then(response => {
+                const tracks = response.tracks.items.map((item: any) => item.track);
+                console.log(JSON.stringify(tracks));
+                setTracks(tracks);
+            })
     }, []);
 
-    const mostLiked = tracks.splice(0, 10);
     return(
         <div className="top-tracks">
             <MostLiked 
-                items={mostLiked}
+                items={tracks.slice(0, 10)}
             />
             <AllTops 
-                tracks={tracks}
+                tracks={tracks.slice(10, tracks.length)}
             />
         </div>
     )
