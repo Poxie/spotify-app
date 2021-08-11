36835320b38b447109334828c93929002603594e
Poxie <poxen12@hotmail.com>
2021-08-11 05:51:00 +0200

    Incresed fetch limit and fixed with context types

--- src/contexts/ProfileProvider.tsx
+++ src/contexts/ProfileProvider.tsx
@@ -1,11 +1,17 @@
-import React, { createContext, useContext } from "react"
+import React, { createContext, useContext, useEffect, useState } from "react"
 import { Redirect } from 'react-router-dom';
+import { Artist } from "../types/Artist";
 import { ProfileContext as ProfileContextType } from "../types/ProfileContext";
+import { Track } from "../types/Track";
+import { useAPI } from "./ApiProvider";
 import { useAuthentication } from "./AuthenticationProvider";
 
 const initial = {
     user: {},
+    artists: [],
+    tracks: []
 }
+// @ts-ignore: because user is never actually an empty object
 const ProfileContext = createContext<ProfileContextType>(initial);
 
 export const useProfile = () => {
@@ -16,13 +22,35 @@ interface Props {
     children: any;
 }
 export const ProfileProvider: React.FC<Props> = ({ children }) => {
+    const { get } = useAPI();
     const { user } = useAuthentication();
+    const [top, setTop] = useState<{tracks: Track[], artists: Artist[]}>({tracks: [], artists: []});
+
+    useEffect(() => {
+        get('me/top/tracks?time_range=long_term&limit=50', true)
+            .then(res => res.json())
+            .then(response => {
+                setTop(previous => {return {...previous, ...{tracks: response.items}}});
+            })
+
+        get('me/top/artists?time_range=long_term&limit=50', true)
+            .then(res => res.json())
+            .then(response => {
+                setTop(previous => {return {...previous, ...{artists: response.items}}});
+            })
+    }, []);
+
     if(!user) return <Redirect to="/authorize" />;
 
+    if(!Object.keys(user).length || !top.tracks.length || !top.artists.length) return <div>loading</div>
+
     const value = {
-        user: user
+        user: user,
+        tracks: top.tracks,
+        artists: top.artists
     }
     return(
+        // @ts-ignore
         <ProfileContext.Provider value={value}>
             {children}
         </ProfileContext.Provider>
