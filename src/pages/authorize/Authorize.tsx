aa2469d7dc234284a94bdb5ef031a81d33941a5b
Poxie <poxen12@hotmail.com>
2021-08-11 02:19:27 +0200

    Made it direct to authflow if code not found

--- src/pages/authorize/Authorize.tsx
+++ src/pages/authorize/Authorize.tsx
@@ -1,30 +1,35 @@
+import { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 // @ts-ignore
 import { CLIENT_SECRET, CLIENT_ID, SPOTIFY_TOKEN_ENDPOINT } from '../../config.json';
+import { useAuthentication } from '../../contexts/AuthenticationProvider';
 
 export const Authorize = () => {
-    const code = new URLSearchParams(window.location.search).get('code');
-    if(!code) return <Redirect to="/" />;
+    const { login } = useAuthentication()
 
-    const data = new URLSearchParams();
-    data.append("grant_type", "authorization_code");
-    data.append("redirect_uri", 'http://localhost:3000/authorize');
-    data.append("code", code);
+    useEffect(() => {
+        const code = new URLSearchParams(window.location.search).get('code');
+        if(!code) return login();
 
-    const encodedClientCredentials = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
-    fetch(`${SPOTIFY_TOKEN_ENDPOINT}?grant_type=authorization_code`, {
-        method: 'POST',
-        body: data,
-        headers: {
-            'Authorization': `Basic ${encodedClientCredentials}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(res => res.json()).then(response => {
-        if(!response.access_token) return;
-        window.localStorage.userAccessToken = response.access_token;
-    }).catch(console.error);
-    
-    window.location.href = window.location.origin;
+        const data = new URLSearchParams();
+        data.append("grant_type", "authorization_code");
+        data.append("redirect_uri", 'http://localhost:3000/authorize');
+        data.append("code", code);
+
+        const encodedClientCredentials = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+        fetch(`${SPOTIFY_TOKEN_ENDPOINT}?grant_type=authorization_code`, {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Authorization': `Basic ${encodedClientCredentials}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(res => res.json()).then(response => {
+            if(!response.access_token) return;
+            window.localStorage.userAccessToken = response.access_token;
+            window.location.href = window.location.origin;
+        }).catch(console.error);
+    }, []);
 
     return(
         <div className="authorize"></div>
