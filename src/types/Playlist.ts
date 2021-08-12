3e5d6e69117f552a31057763e96966bf0f42c911
Poxie <poxen12@hotmail.com>
2021-08-12 09:25:40 +0200

    Only displays owned playlists

--- src/types/Playlist.ts
+++ src/types/Playlist.ts
@@ -1,8 +1,10 @@
 import { Image } from "./Image";
+import { User } from "./User";
 
 export interface Playlist {
     uri: string;
     images: Image[];
     id: string;
     name: string;
+    owner: User;
 }
