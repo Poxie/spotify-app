016e138da5a56e1fb3d396e24d3fd8ab0263805d
Poxie <poxen12@hotmail.com>
2021-08-12 19:24:38 +0200

    Added a close button to the modals

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -1,14 +1,24 @@
+import { useModal } from "../contexts/ModalProvider"
+import { CloseIcon } from "../icons/CloseIcon"
+import { Clickable } from "./Clickable"
+import { Flex } from "./Flex"
+
 interface Props {
     text: string;
     description?: string;
     children?: any;
 }
 export const ModalHeader: React.FC<Props> = ({ text, description, children }) => {
+    const { close } = useModal();
+
     return(
         <div className="modal-header">
-            <div className="title">
+            <Flex className="title" justifyContent={'space-between'} alignItems={'center'}>
                 {text}
-            </div>
+                <Clickable onClick={close}>
+                    <CloseIcon />
+                </Clickable>
+            </Flex>
             {description && (
                 <span className="description">
                     {description}
