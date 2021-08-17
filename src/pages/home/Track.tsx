7051c56d648b155625bba6413135fdcb01851f58
Poxie <poxen12@hotmail.com>
2021-08-17 15:12:26 +0200

    Added animation to top songs

--- src/pages/home/Track.tsx
+++ src/pages/home/Track.tsx
@@ -12,8 +12,18 @@ interface Props {
     index?: number
 }
 export const Track: React.FC<Props> = ({ name, image, artists, duration, uri, index }) => {
+    let style;
+    if(index) {
+        const firstDigit = parseInt(index.toString().slice(0,1));
+        const prevIndex = parseInt(index.toString().slice(1,2)) === 0 ? (firstDigit - 1) * 10 : firstDigit * 10;
+        const delay = ((index - prevIndex) * .05);
+        console.log(delay);
+        style = {
+            animationDelay: `${delay}s`
+        }
+    }
     return(
-        <Flex className="track" alignItems={'center'} justifyContent={'space-between'}>
+        <Flex className="track" alignItems={'center'} justifyContent={'space-between'} style={style}>
             <Flex className="info" alignItems={'center'}>
                 {index && (
                     <span className="track-index">
